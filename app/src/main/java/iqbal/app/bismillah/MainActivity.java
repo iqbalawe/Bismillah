900de7e83fb680dab460ac95180d6932e67bbab8
Iqbal Ajie Wahyudin <iqbalbal25@gmail.com>
2019-01-17 14:21:04 +0700

    Create Bottom Navigation

---
+++ app/src/main/java/iqbal/app/bismillah/MainActivity.java
@@ -0,0 +1,72 @@
+package iqbal.app.bismillah;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import iqbal.app.bismillah.fragment.HomeFragment;
+import iqbal.app.bismillah.fragment.ProfileFragment;
+import iqbal.app.bismillah.fragment.ReminderFragment;
+import iqbal.app.bismillah.fragment.SearchFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = findViewById(R.id.btm_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //load fragment by default
+        toolbar.setTitle("Home");
+        loadFragment(new HomeFragment());
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment;
+            switch (menuItem.getItemId()) {
+                case R.id.home_menu:
+                    toolbar.setTitle("Home");
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.search_menu:
+                    toolbar.setTitle("Search");
+                    fragment = new SearchFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.reminder_menu:
+                    toolbar.setTitle("Reminder");
+                    fragment = new ReminderFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.profile_menu:
+                    toolbar.setTitle("Profile");
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
---
+++ app/src/main/java/iqbal/app/bismillah/fragment/SearchFragment.java
@@ -0,0 +1,20 @@
+package iqbal.app.bismillah.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import iqbal.app.bismillah.R;
+
+public class SearchFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        return view;
+    }
+}
